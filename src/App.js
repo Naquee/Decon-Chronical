fba70f8fda6c1abc40c69531672f7f3491c7c255
Alisha <alishamohasin85@gmail.com>
2022-06-16 20:42:51 +0530

    updated App.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-     
-     <Navbar/>
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
+      <Navbar />
     </div>
   );
 }
